2002fdc5f573874518f625f9e8389c8170c63c2e
Fabio Lucas Maciel <fabio.lucas1403@gmail.com>
2020-09-03 01:08:46 -0300

    delet img inutil ep e ajuste back-dow.js

--- scripts/back-down.js
+++ scripts/back-down.js
@@ -4,7 +4,6 @@ var podcast = document.querySelector('div#podcast')
 
 back.addEventListener("click", function back(){
     history.go(-1)
-    podcast.style.transition =  ".2s"
     podcast.style.height = "0";
     podcast.style.marginTop = "0";
 })
